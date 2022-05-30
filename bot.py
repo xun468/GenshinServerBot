e3bb6b0a3b0ada0e5e2bccc1009dc504fe1183df
xun468 <xun468@yahoo.com>
2022-05-30 18:57:47 +0200

    Added timezone conversion and maybe some other stuff? I forgot

--- bot.py
+++ bot.py
@@ -7,8 +7,27 @@ from discord.utils import get
 import gspread
 import re 
 from datetime import datetime, timedelta
+from pathlib import Path
 import pickle
+import markovify 
+import spacy
+from datetime import datetime, timezone
+from dateutil import tz
 
+nlp = spacy.load("en_core_web_sm")
+def is_image_url(url):
+    parsed = url.split(".")
+    ending = parsed[-1].lower()
+
+    return True if ending == "jpg" or ending == "jpeg" or ending == "png" or ending == "gif" else False
+
+class POSifiedText(markovify.NewlineText):
+    def word_split(self, sentence):
+        return ["::".join((word.orth_, word.pos_)) for word in nlp(sentence)]
+
+    def word_join(self, words):
+        sentence = " ".join(word.split("::")[0] for word in words)
+        return sentence
 
 intents = discord.Intents.all()
 client = commands.Bot(command_prefix="!")
@@ -33,6 +52,10 @@ housing_msg_id = 802955738267385906
 #leaks channels 
 leaks_channel = 913137663400374312
 leaks_discussion = 777083973984976946
+
+#Other channels 
+general = 763500030928879666
+news = 763499148867403816
 
 servers = {
 	EMOJIS[':red_square:']    : "NA",
@@ -63,7 +86,11 @@ vanity = {
 	EMOJIS[':knife:']   : 806554181175214080,
 	"broke"   : 802948687508668456,
 	EMOJIS[':ghost:'] : 865381880232280065,
-	"vengeance" : 865381256027963433
+	"vengeance" : 865381256027963433,
+	EMOJIS[':fox:'] : 963046824275492866,
+	EMOJIS[':bubble_tea:'] : 963088831429611560, 
+	"baalstare" : 963087112364765314,
+	EMOJIS[':snowflake:'] : 967189618606895184,
 
 }
 
@@ -79,6 +106,19 @@ server_names = ["NA", "EU", "Asia"]
 WL_server = ["EU1-5", "EU6" ,"EU7",	"EU8",
 			"NA1-5", "NA6", "NA7", "NA8",
 			"Asia1-5", "Asia6", "Asia7", "Asia8"]
+
+
+fanart_contest = {
+	"paimoncoinbobross" : 922678865707565086,
+	"paimoncoincute" : 909272962119651378,
+	"paimoncoincool" : 909273025361371157, 
+	"paimoncoinfancy" : 909273086354935820,
+}
+
+with open('json_model.json', 'r') as outfile:
+    model_json = outfile.read()
+text_model = POSifiedText.from_json(model_json)
+chat_count = 0 
 
 def get_server(name, guild):
 	if name in servers.keys():
@@ -158,6 +198,7 @@ async def on_raw_reaction_add(payload):
 	channel = payload.channel_id	
 	if channel == fanart_source:
 		message_id = payload.message_id
+		member_id = payload.member.id
 		msg = await client.get_channel(payload.channel_id).fetch_message(message_id)
 
 		seen = set()			
@@ -166,36 +207,83 @@ async def on_raw_reaction_add(payload):
 				seen.add(user)
 
 		print(len(seen))
-		if len(seen) >= 4 or payload.member.id == grandpapants:
+		if len(seen) >= 4 or member_id == grandpapants:
 			with open('MuseumIDs.txt') as f:
 				lines = [line.rstrip() for line in f]
-			if "twitter.com" in msg.content:	
-				print("in twitter")	
-			
-				if str(message_id) not in lines:
-					print("Image has been voted into the museum") 	
+
+			if str(message_id) not in lines:	
+				print("Image has been voted into the museum")
+
+				if "twitter.com" in msg.content:	
+					print("in twitter")									
 					embed = msg.content
 					await client.get_channel(fanart_dest).send(embed)
-					with open('MuseumIDs.txt', "a") as f:
-						f.write(str(message_id)+"\n")		
-			elif msg.embeds:
-				print("in embeds")	
+					
+				elif is_image_url(msg.content):
+					print("is image link")
+					await client.get_channel(fanart_dest).send(msg.content)
 
-				if str(message_id) not in lines:
-					print("Image has been voted into the museum") 	
+				elif msg.embeds:
+					print("in embeds")	
 					for embed in msg.embeds:
 						await client.get_channel(fanart_dest).send(embed=embed)
-					with open('MuseumIDs.txt', "a") as f:
-						f.write(str(message_id)+"\n")
-			elif msg.attachments:	
-				print("in attachments")	
-
-				if str(message_id) not in lines:
-					print("Image has been voted into the museum") 
+					
+				elif msg.attachments:	
+					print("in attachments")
 					for embed in msg.attachments:
-						await client.get_channel(fanart_dest).send(embed)
-					with open('MuseumIDs.txt', "a") as f:
+						await client.get_channel(fanart_dest).send(embed)					
+
+				with open('MuseumIDs.txt', "a") as f:
 						f.write(str(message_id)+"\n")
+
+
+		#fanart contest		
+		if payload.emoji.name in fanart_contest.keys():
+			coin_name = payload.emoji.name
+			target_channel = fanart_contest[coin_name]
+
+			filename = Path(coin_name + "-ImageIDs.txt")
+			filename.touch(exist_ok=True)
+
+			filename = Path(coin_name + "-MemberIDs.txt")
+			filename.touch(exist_ok=True)
+			
+
+			with open(coin_name + "-ImageIDs.txt") as f:
+				image_ids = [line.rstrip() for line in f]
+			with open(coin_name + "-MemberIDs.txt") as f:
+				user_ids = [line.rstrip() for line in f]
+
+			if str(message_id) not in image_ids and str(member_id) not in user_ids:
+				print(coin_name + " nomination has been made")
+				if "twitter.com" in msg.content:	
+					print("in twitter")									
+					embed = msg.content
+					new = await client.get_channel(target_channel).send(embed)
+					
+				elif is_image_url(msg.content):
+					print("is image link")
+					new = await client.get_channel(target_channel).send(msg.content)
+
+				elif msg.embeds:
+					print("in embeds")			
+					new = await client.get_channel(target_channel).send(embed=msg.embeds[0])
+					
+				elif msg.attachments:	
+					print("in attachments")
+					new = await client.get_channel(target_channel).send(msg.attachments[0])	
+
+				await new.add_reaction("<:paimoncoin:922681177071034368>")				
+
+				with open(coin_name + "-ImageIDs.txt", "a") as f:
+					f.write(str(message_id)+"\n")
+				with open(coin_name + "-MemberIDs.txt","a") as f:
+					f.write(str(member_id)+"\n")
+				
+
+				
+
+
 
 		
 @client.event 
@@ -225,28 +313,25 @@ async def on_raw_reaction_remove(payload):
 			else: 
 				print("member not found")
 
+
 @client.event
 async def on_message(message):
     channel = message.channel
+    await client.process_commands(message)
+    global chat_count
 
     if channel.id == leaks_channel:
     	print("leak detected")
+    	await client.get_channel(leaks_discussion).send(message.content, tts=message.tts, files=[await attch.to_file() for attch in message.attachments])
+    if channel.id == general: 
+    	chat_count = chat_count + 1 
+    	if chat_count > 200: 
+    		await client.get_channel(general).send(text_model.make_sentence())
+    		chat_count = 0
+    if channel.id == news: 
+    	if len(message.content) == 12: 
+    		await client.get_channel(news).send("<https://genshin.hoyoverse.com/en/gift?code=" + message.content+">")
 
-    	if len(message.content) != 0: 
-    		await client.get_channel(leaks_discussion).send(message.content)
-
-    	if message.embeds: 
-    		for e in message.embeds: 
-    			await client.get_channel(leaks_discussion).send(embed=e)    			
-
-    	if len(message.attachments) > 0:
-    		for a in message.attachments: 
-    			if a.is_spoiler():
-    				a.filename = f"SPOILER_{a.filename}"
-    				a = await a.to_file()
-    				await client.get_channel(leaks_discussion).send(file=a)
-    			else:
-    				await client.get_channel(leaks_discussion).send(a)
 
 @client.command()
 async def register(ctx, uid = None, server = None, wl = None):
@@ -255,7 +340,9 @@ async def register(ctx, uid = None, server = None, wl = None):
 	#automatic 
 	if uid and len(uid) == 9 and uid.isnumeric(): 		
 		#get roles 
-		all_roles = set([i.name for i in user.roles])
+		all_roles = [] 
+		if user.roles:
+			all_roles = set([i.name for i in user.roles])
 		print(str(user) + " registering with " + str(all_roles))
 
 		user_s = str(user).lower()
@@ -311,6 +398,7 @@ async def register(ctx, uid = None, server = None, wl = None):
 
 @client.command()
 async def goon(ctx, term = None):
+	print("goon")
 	sheet = gc.open_by_key('1E9KJhGEjgST2KdPBDy0UcO2KvYRq4YXjtTJmctCGvwQ').sheet1
 	#get self
 	if term == None: 
@@ -362,68 +450,106 @@ async def unregister(ctx, uid = None):
 			sheet.delete_row(r.row)
 	return 
 
-@loop(minutes=60.0)
-async def countdown():
-	print("Checking banned list")
-	try:
-		ban_list = pickle.load(open("ban_list", 'rb'))
-		day_list = pickle.load(open("day_list", 'rb'))
+@client.command()
+async def aprilfools(ctx, term = None):
+	if ctx.channel.id == 851544672672677958:
+		if term:
+			try: 
+				msg = text_model.make_sentence_with_start(term)
+			except markovify.text.ParamError:
+				msg = text_model.make_sentence()
+			await client.get_channel(general).send(msg)
+		else: 
+			await client.get_channel(general).send(text_model.make_sentence())	
+	
+@client.command()
+async def time(ctx, date = None, hour = None):
+	message = ""
+	from_zone = tz.gettz("UTC+8")
 
-		now = datetime.now()
-		to_remove = [] 
-
-		for i in range(len(ban_list)):
-			if(now > day_list[i]):
-				print(ban_list[i])
-
-				guild = await client.fetch_guild(763498760537767956)
-
-				try: 
-					member = await guild.fetch_member(ban_list[i])
-				except: 
-					member = False 
-
-				if member:
-					print("checking " + str(member))
-					await member.remove_roles(discord.utils.get(guild.roles,id=854734771321569290))
-					channel = await client.fetch_channel(782784858471792641)
-					to_remove.append(i)
-					await channel.send("Unprobed " + str(member))
-
-		for i in to_remove: 
-			del ban_list[i]
-			del day_list[i]
-
-		pickle.dump(ban_list, open("ban_list", "wb"))
-		pickle.dump(day_list, open("day_list", "wb"))
-
-	except(OSError, IOError) as e:
-		print("No ban list found")
-
-
-@client.command(pass_context = True)
-async def probe(ctx,member:discord.Member, days = 1, hours = 0):
-	if ctx.message.author.id == grandpapants or ctx.message.author.id == xun:
-		guild = ctx.guild
-		await member.add_roles(discord.utils.get(guild.roles,id=854734771321569290))
-		await ctx.send('User probed for **' + str(days) + ' day(s)** and **' + str(hours) + ' hour(s)**')
-		print("probed " + str(member) + " for " + str(days) + " days and " + str(hours) + " hours")
-
-		try: 
-			ban_list = pickle.load(open("ban_list", 'rb'))
-			day_list = pickle.load(open("day_list", 'rb'))
-		except (OSError, IOError) as e:
-			ban_list = []
-			day_list = [] 
-
-		ban_list.append(member.id)
-		day_list.append(datetime.now() + timedelta(days=days, hours = hours))
-
-
-		pickle.dump(ban_list, open("ban_list", "wb"))
-		pickle.dump(day_list, open("day_list", "wb"))
-
+	if date == None:
+		message = "No date entered. Find out how long until a specific UTC+8 time! Format is something like 05/31 6AM"
 	else: 
-		await ctx.send("Thank you for volunteering for mod duty. Please report to Grandpapants as soon as possible")
-countdown.start()
+		try:
+			now = datetime.now(timezone.utc)
+
+			combined = str(now.year) + " " + date + " " + hour
+			target_date = datetime.strptime(combined, '%Y %m/%d %I%p')
+			target_date = target_date.replace(tzinfo=from_zone)
+			print(target_date)
+
+			td = target_date - datetime.now(timezone.utc)
+
+			print(td)
+
+			message = "Time until " + date + " " + hour + " UTC+8: " + str(td.days) + " days " + str(td.seconds//3600) + " hours " + str((td.seconds//60)%60) + " minutes"
+		except ValueError:
+			print(combined)
+			message = "Invalid date format, please use something like 05/31 6AM"
+
+	await ctx.send(message)
+
+		
+# @loop(minutes=60.0)
+# async def countdown():
+# 	print("Checking banned list")
+# 	try:
+# 		ban_list = pickle.load(open("ban_list", 'rb'))
+# 		day_list = pickle.load(open("day_list", 'rb'))
+
+# 		now = datetime.now()
+# 		to_remove = [] 
+
+# 		for i in range(len(ban_list)):
+# 			if(now > day_list[i]):
+# 				guild = await client.fetch_guild(763498760537767956)
+
+# 				try: 
+# 					member = await guild.fetch_member(ban_list[i])
+# 				except: 
+# 					member = False 
+
+# 				if member:
+# 					print("checking " + str(member))
+# 					await member.remove_roles(discord.utils.get(guild.roles,id=854734771321569290))
+# 					channel = await client.fetch_channel(782784858471792641)
+# 					to_remove.append(i)
+# 					await channel.send("Unprobed " + str(member))
+
+# 		for i in to_remove: 
+# 			del ban_list[i]
+# 			del day_list[i]
+
+# 		pickle.dump(ban_list, open("ban_list", "wb"))
+# 		pickle.dump(day_list, open("day_list", "wb"))
+
+# 	except(OSError, IOError) as e:
+# 		print("No ban list found")
+
+
+# @client.command(pass_context = True)
+# async def probe(ctx,member:discord.Member, days = 1, hours = 0):
+# 	if ctx.message.author.id == grandpapants or ctx.message.author.id == xun:
+# 		guild = ctx.guild
+# 		await member.add_roles(discord.utils.get(guild.roles,id=854734771321569290))
+# 		await ctx.send('User probed for **' + str(days) + ' day(s)** and **' + str(hours) + ' hour(s)**')
+# 		print("probed " + str(member) + " for " + str(days) + " days and " + str(hours) + " hours")
+
+# 		try: 
+# 			ban_list = pickle.load(open("ban_list", 'rb'))
+# 			day_list = pickle.load(open("day_list", 'rb'))
+# 		except (OSError, IOError) as e:
+# 			ban_list = []
+# 			day_list = [] 
+
+# 		ban_list.append(member.id)
+# 		day_list.append(datetime.now() + timedelta(days=days, hours = hours))
+
+
+# 		pickle.dump(ban_list, open("ban_list", "wb"))
+# 		pickle.dump(day_list, open("day_list", "wb"))
+
+# 	else: 
+# 		await ctx.send("Thank you for volunteering for mod duty. Please report to Grandpapants as soon as possible")
+# countdown.start()
 client.run(config.bot_key)
